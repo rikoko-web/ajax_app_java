df5719c67efc2d88f07a31da1194a721becc1d2e
rikoko-web <e.katayama197@kntct.com>
2026-06-08 14:31:03 +0900

    sql

--- src/main/java/in/tech_camp/ajax_app_java/repository/PostRepository.java
+++ src/main/java/in/tech_camp/ajax_app_java/repository/PostRepository.java
@@ -1,24 +1,22 @@
 package in.tech_camp.ajax_app_java.repository;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
-
 import in.tech_camp.ajax_app_java.entity.PostEntity;
 
 @Mapper
 public interface PostRepository {
-  @Select("select * from posts order by created_at desc")
-  List<PostEntity> findAll();
+    @Select("select * from posts order by created_at desc")
+    List<PostEntity> findAll();
 
-  @Insert("insert into posts (content) values (#{content})")
-   @Options(useGeneratedKeys = true, keyProperty = "id")
-  void insert(PostEntity post);
+    // 💡 PostgreSQL用に「RETURNING id」を明示し、keyColumnを指定する確実な方法に変更しました
+    @Insert("insert into posts (content) values (#{content}) returning id")
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    void insert(PostEntity post);
 
-  
-  @Select("SELECT * FROM posts WHERE id = #{id}")
-  PostEntity findById(int id);
+    @Select("SELECT * FROM posts WHERE id = #{id}")
+    PostEntity findById(int id);
 }
